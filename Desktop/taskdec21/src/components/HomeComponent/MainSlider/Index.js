ac9b9f53a3a99440cd4a25bec46a21923f96e24e
ibrahimsadixovv <tu885t073@code.edu.az>
2022-12-21 04:01:39 +0400

    task react done

---
+++ Desktop/taskdec21/src/components/HomeComponent/MainSlider/Index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./index.scss"
+
+function MainSlider() {
+  return (
+    <div className='MainSlider'>
+    
+    <div className='createAccount'>
+
+    <p> <a> Create an account for better product support!</a> </p>
+
+    </div>
+    
+    <div className='main'>
+
+
+
+    </div>
+
+    </div>
+  )
+}
+
+export default MainSlider
---
+++ Desktop/taskdec21/src/components/HomeComponent/SlickSlider/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SlickSlider
+() {
+  return (
+    <div>SlickSlider
+        
+    </div>
+  )
+}
+
+export default SlickSlider
